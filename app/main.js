cd8bb1c8224b05b34bfa86651b1696118c34f93b
Arthur Battut <arthur.battut@ekino.com>
2018-01-11 16:30:39 +0100

    display images from giphy after research

--- app/main.js
+++ app/main.js
@@ -1,17 +1,31 @@
 import Worker from './workers/gr.worker.js'
 
 const searchInput = document.getElementById('search')
+const resultsHolder = document.getElementById('results')
 
-if (Worker) {
-  const myWorker = new Worker()
+const formatGiphyData = gifArray => {
+  let imgString = ''
+  for (let gif of gifArray) {
+    imgString = imgString.concat(`<img src="${gif.images.original.url}">`)
+  }
+  return imgString
+}
+
+if (self.Worker) {
+  const grWorker = new Worker()
 
   searchInput.addEventListener('keypress', {
     handleEvent: event => {
       if (event.keyCode === 13) { 
         console.log(`submitted input with value: ${searchInput.value}`)
-        myWorker.postMessage(searchInput.value)
+        grWorker.postMessage(searchInput.value)
       }
     }
   })
+  
+  grWorker.onmessage = e => {
+    console.log(e.data)
+    resultsHolder.innerHTML = formatGiphyData(e.data)
+  }
 }
 
--- app/workers/gr.worker.js
+++ app/workers/gr.worker.js
@@ -1,5 +1,17 @@
+import request from 'axios'
+import config from './../../config.json'
+
 onmessage = function(e) {
-  console.log('Message received from main script')
   console.log('Request Giphy API')
+
+  request({
+    method: 'get',
+    url: 'http://' + config.giphy.host + config.giphy.endpoints.search,
+    params: {
+      api_key: config.giphy.apikey,
+      q: e.data,
+      limit: 20
+    } 
+  }).then(response => postMessage(response.data.data)).catch(error => console.log(error))
 }
 
