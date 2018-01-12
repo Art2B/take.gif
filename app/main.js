fcb844ea3aa619006f70a6be5ac66a01cc1f088e
Arthur Battut <arthur.battut@ekino.com>
2018-01-12 11:03:13 +0100

    add favorties to save gif locally

--- app/main.js
+++ app/main.js
@@ -2,26 +2,52 @@ import Worker from './workers/gr.worker.js'
 
 const searchInput = document.getElementById('search')
 const resultsHolder = document.getElementById('results')
+let favButtons = document.querySelector('.gif-holder button')
+
+const addToFav = e => {
+  let slug = e.target.dataset.gif
+  let favs
+  
+  try {
+    favs = JSON.parse(localStorage.getItem('favs'))
+  } catch(error) {
+    favs = new Array()
+  }
+  
+  favs.push(slug)
+  localStorage.setItem('favs', JSON.stringify(favs))
+}
 
 const formatGiphyData = gifArray => {
-  let imgString = ''
-  for (let gif of gifArray) {
-    imgString = imgString.concat(`<img src="${gif.images.original.url}">`)
+  const getImgHtmlString = (gif) => {
+    return (`
+      <div class="gif-holder">
+        <button data-gif="${gif.slug}">Add to fav</button>
+        <img src="${gif.images.original.url}">    
+      </div>  
+    `)
   }
+
+  let imgString = ''
+    for (let gif of gifArray) {
+      // imgString = imgString.concat(`<img src="${gif.images.original.url}">`)
+      imgString = imgString.concat(getImgHtmlString(gif))
+    }
   return imgString
 }
 
 if (self.Worker) {
   const grWorker = new Worker()
 
-  searchInput.addEventListener('keypress', {
-    handleEvent: event => {
-      if (event.keyCode === 13) grWorker.postMessage(searchInput.value)
-    }
-  })
-  
+    searchInput.addEventListener('keypress', {
+      handleEvent: event => {
+        if (event.keyCode === 13) grWorker.postMessage(searchInput.value)
+      }
+    })
+
   grWorker.onmessage = e => {
     resultsHolder.innerHTML = formatGiphyData(e.data)
+    favButtons = document.querySelector('.gif-holder button')
+    favButtons.addEventListener('click', addToFav)
   }
 }
-
