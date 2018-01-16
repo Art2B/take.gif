8550c6a2f169c03e18eed822a6f490eaa368371f
Arthur Battut <arthur.battut@ekino.com>
2018-01-16 17:08:53 +0100

    Add react to project

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,10 +4,13 @@ import CleanWebpackPlugin from 'clean-webpack-plugin'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import FaviconsWebpackPlugin from 'favicons-webpack-plugin'
 
+const APP_DIR = path.resolve(__dirname, 'app')
+const BUILD_DIR = path.resolve(__dirname, 'build')
+
 export default {
   entry: './app/main.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: BUILD_DIR,
     filename: 'main.bundle.js'
   },
   devtool: 'inline-source-map',
@@ -33,6 +36,10 @@ export default {
         fallback: 'style-loader',
         use: ['css-loader', 'sass-loader']
       })
+    }, {
+      test: /\.jsx?/,
+      include: APP_DIR,
+      loader: 'babel-loader'
     }]
   }
 }
